971211c686a67301e2dbf4ebebda9c96074c040a
Fred Medlin <fmedlin@gmail.com>
2015-12-09 20:35:22 -0500

    Viewpager scaffold

--- app/src/main/java/com/fmedlin/digest/TourActivity.java
+++ app/src/main/java/com/fmedlin/digest/TourActivity.java
@@ -1,13 +1,124 @@
 package com.fmedlin.digest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
 
 public class TourActivity extends AppCompatActivity {
+
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour);
+
+        pager = ButterKnife.findById(this, R.id.viewpager);
+        pager.setAdapter(new TourPagerAdapter(this));
     }
+
+    class TourPagerAdapter extends PagerAdapter {
+
+        Context context;
+
+        public TourPagerAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            TourScreen screen = null;
+
+            switch(position) {
+                case 0:
+                    screen = new TourScreenOne(context);
+                    break;
+
+                case 1:
+                    screen = new TourScreenTwo(context);
+                    break;
+
+                case 2:
+                    screen = new TourScreenThree(context);
+                    break;
+
+                default:
+                    break;
+            }
+
+            ViewGroup layout = (ViewGroup) LayoutInflater.from(context).inflate(screen.getLayoutResId(), container, false);
+            container.addView(layout);
+            return layout;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+    }
+
+    abstract class TourScreen {
+        Context context;
+
+        public TourScreen(Context context) {
+            this.context = context;
+        }
+
+        abstract @LayoutRes public int getLayoutResId();
+    }
+
+    public class TourScreenOne extends TourScreen {
+
+        public TourScreenOne(Context context) {
+            super(context);
+        }
+
+        @Override
+        public int getLayoutResId() {
+            return R.layout.view_tour_page1;
+        }
+    }
+
+    public class TourScreenTwo extends TourScreen {
+
+        public TourScreenTwo(Context context) {
+            super(context);
+        }
+
+        @Override
+        public int getLayoutResId() {
+            return R.layout.view_tour_page2;
+        }
+    }
+
+    public class TourScreenThree extends TourScreen {
+
+        public TourScreenThree(Context context) {
+            super(context);
+        }
+
+        @Override
+        public int getLayoutResId() {
+            return R.layout.view_tour_page3;
+        }
+    }
+
 }
